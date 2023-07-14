3af1a32541449cb0e99c1f2de0485a9071e985fc
starshb <gksquf7367@naver.com>
2023-07-14 23:29:56 +0900

    first commit

---
+++ src/test/java/com/example/pharmacy_Find/PharmacyFindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy_Find;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyFindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
